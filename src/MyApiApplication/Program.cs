76eb5c9ee543ebd895b9d41250d38344bffc1d8e
Luca Biffi <lubiffi@yahoo.it>
2024-09-22 13:51:09 +0200

    Added NLog

--- src/MyApiApplication/Program.cs
+++ src/MyApiApplication/Program.cs
@@ -1,4 +1,7 @@
 
+using NLog.Extensions.Logging;
+using NLog.Web;
+
 namespace MyApiApplication {
 	public class Program {
 		public static void Main(string[] args) {
@@ -10,6 +13,10 @@ namespace MyApiApplication {
 			// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 			builder.Services.AddEndpointsApiExplorer();
 			builder.Services.AddSwaggerGen();
+
+			builder.Logging.ClearProviders();
+			builder.Host.UseNLog();
+			builder.Services.AddSingleton<ILoggerProvider, NLogLoggerProvider>();
 
 			var app = builder.Build();
 
